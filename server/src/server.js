b1ff351cce3c94a7f36a1b5f3304d307794f6f46
giaaniM <giaaniM@users.noreply.github.com>
2026-02-12 14:41:07 +0100

    fix: remove typo in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
-    app.get可以在('/(.*)', (req, res) => {
+    app.get('/(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
 }
